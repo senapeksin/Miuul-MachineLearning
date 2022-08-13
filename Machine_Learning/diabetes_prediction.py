d8f7dc5d74223efcf769a97129b9e84c23547d0c
senapeksin <senapksnn@gmail.com>
2022-08-14 01:13:26 +0300

    pipeline tamamlandı

--- Machine_Learning/diabetes_prediction.py
+++ Machine_Learning/diabetes_prediction.py
@@ -5,20 +5,140 @@
 import joblib
 import pandas as pd
 
+import joblib
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
 df = pd.read_csv("Machine_Learning/datasets/diabetes.csv")
 
 random_user = df.sample(1, random_state=45)
 
-new_model = joblib.load("voting_clf.pkl")
+new_model = joblib.load("Machine_Learning/voting_clf.pkl")
 
 new_model.predict(random_user)
 
 from diabetes_pipeline import diabetes_data_prep
 
+# Data Preprocessing & Feature Engineering
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    """
+
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+    Not: Kategorik değişkenlerin içerisine numerik görünümlü kategorik değişkenler de dahildir.
+
+    Parameters
+    ------
+        dataframe: dataframe
+                Değişken isimleri alınmak istenilen dataframe
+        cat_th: int, optional
+                numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+        car_th: int, optinal
+                kategorik fakat kardinal değişkenler için sınıf eşik değeri
+
+    Returns
+    ------
+        cat_cols: list
+                Kategorik değişken listesi
+        num_cols: list
+                Numerik değişken listesi
+        cat_but_car: list
+                Kategorik görünümlü kardinal değişken listesi
+
+    Examples
+    ------
+        import seaborn as sns
+        df = sns.load_dataset("iris")
+        print(grab_col_names(df))
+
+
+    Notes
+    ------
+        cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+        num_but_cat cat_cols'un içerisinde.
+        Return olan 3 liste toplamı toplam değişken sayısına eşittir: cat_cols + num_cols + cat_but_car = değişken sayısı
+
+    """
+
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    # print(f"Observations: {dataframe.shape[0]}")
+    # print(f"Variables: {dataframe.shape[1]}")
+    # print(f'cat_cols: {len(cat_cols)}')
+    # print(f'num_cols: {len(num_cols)}')
+    # print(f'cat_but_car: {len(cat_but_car)}')
+    # print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+
+def diabetes_data_prep(dataframe):
+    dataframe.columns = [col.upper() for col in dataframe.columns]
+
+    # Glucose
+    dataframe['NEW_GLUCOSE_CAT'] = pd.cut(x=dataframe['GLUCOSE'], bins=[-1, 139, 200], labels=["normal", "prediabetes"])
+
+    # Age
+    dataframe.loc[(dataframe['AGE'] < 35), "NEW_AGE_CAT"] = 'young'
+    dataframe.loc[(dataframe['AGE'] >= 35) & (dataframe['AGE'] <= 55), "NEW_AGE_CAT"] = 'middleage'
+    dataframe.loc[(dataframe['AGE'] > 55), "NEW_AGE_CAT"] = 'old'
+
+    # BMI
+    dataframe['NEW_BMI_RANGE'] = pd.cut(x=dataframe['BMI'], bins=[-1, 18.5, 24.9, 29.9, 100],
+                                        labels=["underweight", "healty", "overweight", "obese"])
+
+    # BloodPressure
+    dataframe['NEW_BLOODPRESSURE'] = pd.cut(x=dataframe['BLOODPRESSURE'], bins=[-1, 79, 89, 123],
+                                            labels=["normal", "hs1", "hs2"])
+
+    cat_cols, num_cols, cat_but_car = grab_col_names(dataframe, cat_th=5, car_th=20)
+
+    cat_cols = [col for col in cat_cols if "OUTCOME" not in col]
+
+    df = one_hot_encoder(dataframe, cat_cols, drop_first=True)
+
+    cat_cols, num_cols, cat_but_car = grab_col_names(df, cat_th=5, car_th=20)
+
+    replace_with_thresholds(df, "INSULIN")
+
+    X_scaled = StandardScaler().fit_transform(df[num_cols])
+    df[num_cols] = pd.DataFrame(X_scaled, columns=df[num_cols].columns)
+
+    y = df["OUTCOME"]
+    X = df.drop(["OUTCOME"], axis=1)
+
+    return X, y
+
+
 X, y = diabetes_data_prep(df)
 
 random_user = X.sample(1, random_state=50)
 
-new_model = joblib.load("voting_clf.pkl")
+new_model = joblib.load("Machine_Learning/voting_clf.pkl")
 
 new_model.predict(random_user)
